90c8b3ff10af2c2effccc09208a4443c80083ec5
ebcrowder <ebcrowder@gmail.com>
2018-11-18 20:43:46 -0800

    remove d3 and add victory

--- components/Chart.js
+++ components/Chart.js
@@ -1,123 +1,31 @@
-import React, { PureComponent, createRef } from 'react';
-import styled from 'styled-components';
-import { easeCubicOut, extent, line, scaleLinear, scaleTime, select } from 'd3';
-import interpolatePath from 'd3-interpolate-path';
+import React, { Component } from 'react';
+import { VictoryChart, VictoryTheme, VictoryLine } from 'victory';
 
-const LINE_DUMMY = Array(2)
-  .fill()
-  .map((a, i) => ({ price: 0, time: new Date(2010 + i) }));
-const PADDING = 24;
-const TRANSITION_DURATION = 500;
-
-const safePrices = prices =>
-  Array.isArray(prices) && prices.length > 1 ? prices : LINE_DUMMY;
-
-const Svg = styled.svg`
-  height: 100%;
-  width: 100%;
-  pointer-events: none;
-  flex: 1 0 ${() => 4 * 40}rem;
-`;
-
-export default class LineBase extends PureComponent {
-  pathRef = createRef();
-  svgRef = createRef();
-
-  componentDidMount() {
-    if (
-      this.pathRef &&
-      this.pathRef.current &&
-      this.svgRef &&
-      this.svgRef.current
-    ) {
-      const { height, width } = this.svgRef.current.getBoundingClientRect();
-      const { prices } = this.props;
-      this.path = select(this.pathRef.current);
-
-      this.height = height;
-      this.width = width;
-
-      const d = lineFromPrices(
-        scalePrices(safePrices(prices), height, width, PADDING, PADDING)
-      );
-      this.path.attr('d', d);
-      this.d = d;
-
-      window.addEventListener('resize', this.handleResize);
-    }
-  }
-
-  componentDidUpdate() {
-    this.updatePath();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize = () => {
-    if (this.svgRef && this.svgRef.current) {
-      const { height, width } = this.svgRef.current.getBoundingClientRect();
-      this.height = height;
-      this.width = width;
-
-      this.updatePath();
-    }
-  };
-
-  updatePath = () => {
-    const scalePrices = (
-      data,
-      height,
-      width,
-      paddingTop = 0,
-      paddingBottom = 0,
-      paddingLeft = 0,
-      paddingRight = 0
-    ) => {
-      const priceToY = scaleLinear()
-        .range([height - paddingBottom, paddingTop])
-        .domain(extent(data, d => d.price));
-
-      const timeToX = scaleTime()
-        .range([paddingLeft, width - paddingRight])
-        .domain(extent(data, d => d.time));
-
-      return data.map(({ price, time }) => ({
-        price: priceToY(price),
-        time: timeToX(time)
-      }));
-    };
-
-    const lineFromPrices = line()
-      .x(d => d.time)
-      .y(d => d.price);
-
-    const { prices } = this.props;
-
-    const d = lineFromPrices(
-      scalePrices(safePrices(prices), this.height, this.width, PADDING, PADDING)
-    );
-
-    this.path
-      .transition()
-      .duration(TRANSITION_DURATION)
-      .ease(easeCubicOut)
-      .attrTween('d', interpolatePath.bind(null, this.d, d));
-
-    this.d = d;
-  };
+export default class LineBase extends Component {
+  formatPriceHistory = data =>
+    data
+      .map(d => ({
+        price: Number(d.price),
+        time: new Date(d.time).toLocaleString()
+      }))
+      .sort((a, b) => a.time - b.time);
 
   render() {
+    const formattedPrices = this.formatPriceHistory(this.props.prices);
+    console.log(formattedPrices);
     return (
-      <Svg innerRef={this.svgRef}>
-        <path
-          fill="none"
-          ref={this.pathRef}
-          stroke="#FF3D00"
-          strokeWidth="1.5"
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: '#c43a31' },
+            parent: { border: '1px solid #ccc' }
+          }}
+          y={d => d.price}
+          x={d => d.time}
+          data={formattedPrices}
+          animate={{ duration: 2000 }}
         />
-      </Svg>
+      </VictoryChart>
     );
   }
 }
